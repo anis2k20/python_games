07c0b318b420f1ef000858545ac49d5213da528f
Anis <anis2k20@gmail.com>
2023-10-01 00:50:55 +0600

    project complete

--- The Minion Game/main.py
+++ The Minion Game/main.py
@@ -1,60 +1,44 @@
-from itertools import combinations
+
 from itertools import permutations
+import re
 
 def minion_game(string):
     vowel = ["a", "e", "i", "o", "u"]
 
     perms = set()
     stuart = []
+    kevin = []
+    stuart_score = 0
+    kevin_score = 0
 
     for i in range(1, len(string) + 1):
         for c in permutations(string, i):
             perms.add("".join(c))
-    count = 0
+
     for i in perms:
         if i in string:
             if i[0].lower() in vowel:
                 if i in string:
-                    pass
+                    kevin.append(i)
             else:
                 stuart.append(i)
 
-
+    # calculation for STUART-----------
     for i in (sorted(stuart,key=len)):
-        if i in string:
-            count += string.count(i)
+        regex = f"(?={i})"
+        res = len(re.findall(regex, string))
+        stuart_score += res
 
-    print(count)
+    #calculation for KEVIN-----------
+    for i in (sorted(kevin,key=len)):
+        regex = f"(?={i})"
+        res = len(re.findall(regex, string))
+        kevin_score += res
 
-
-
-    # for c in perms:
-    #     if c[0].lower() in vowel:
-    #         count +=1
-    #         print(c)
-    # print(count)
-
-
-
-
-    # def stuart(string_list):
-    #     count = 0
-    #     # print(string_list)
-    #     for i in string_list:
-    #         if i.lower()[0] in vowel:
-    #             print(i)
-    #             count += 1
-    #     print(count)
-
-
-    # l=[]
-    # [l.append(i) for i in sub_string if i not in l]
-    # stuart(l)
-
-
-#[''.join(l) for i in range(len(x)) for l in combinations(x, i+1)]
-
-
+    if stuart_score>kevin_score:
+        print(f"Stuart {stuart_score}")
+    else:
+        print(f"Kevin {kevin_score}")
 
 s = "BANANA"
 minion_game(s)
