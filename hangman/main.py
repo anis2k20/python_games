80815dad09da28a46f58909f9e29b561d2c7097a
Anis <anis2k20@gmail.com>
2023-10-25 00:05:18 +0600

    user guessing latter and word travers matching done

--- hangman/main.py
+++ hangman/main.py
@@ -13,5 +13,31 @@ for i in range(word_length-2):
 print(f"Word hint: {random_word[0]} {underscore} {random_word[-1]}")
 # End random word hints
 
+# latter matching code
+def word_match():
+    attempt=10
+
+    dash_count = len(random_word)
+    word_travers = 0
+    for i in range(attempt):
+        # user input
+        user_input = input("Enter guessing latter: ")
+
+
+        if user_input == random_word[word_travers]:
+            dash_count -= 1
+            word_travers +=1
+            dash=""
+            for i in range(dash_count):
+                dash +="_ "
+            print(f"{random_word[:word_travers]} {dash}")
+
+        else:
+            print("not exist")
+
+
+art_instruction = word_match()
+# print(art_instruction)
+
 
 
