687d886ce7ce453251ff88fe4a75e8d892d49618
Anis <anis2k20@gmail.com>
2023-10-27 16:10:23 +0600

    Draft for 21 number game

--- main.py
+++ main.py
@@ -1,11 +1,64 @@
 import random
-# import re
-#
-# s = 'ababababa'
-#
-# i="aba"
-# regex = f"(?={i})"
-# res= len(re.findall(regex, s))
-#
-# print("Number of substrings", res)
-print(random.randint(1,2))
+
+NEAR = 4
+container = []
+# first_chance---------------------
+def first_chance():
+    # check consecutive
+    def check_consecutive(c):
+        return sorted(c) == list(range(min(c),max(c)+1))
+
+    # input function--------------
+    def input_func(x):
+        my_values = []
+        for i in range(x):
+            n = int(input("Enter value: "))
+            my_values.append(n)
+
+        is_consecutive = check_consecutive(my_values)
+        print(type(is_consecutive))
+        if is_consecutive is True:
+            container.append(my_values)
+            second_chance()
+        else:
+            disqualified()
+
+
+
+    print("Your turn.")
+    input_amount = int(input("How many number do you want as a input?: "))
+
+    if input_amount<4:
+        input_func(input_amount)
+
+    elif input_amount>=4:
+        disqualified()
+
+
+
+
+
+# Second chance---------------------
+def second_chance():
+    print("I am second chance")
+
+
+
+# disqualified----------------------
+def disqualified():
+    print("You are disqualified")
+
+
+
+# Game initial point----------------
+print("Player 2 is computer.")
+ask = input("Do you want to play 21 Number Game? (Y/N): ").lower()
+choice = input("Do you want first chance or second chance(F/S): ").lower()
+
+if choice == "f":
+    first_chance()
+elif choice == "s":
+    second_chance()
+
+# Testing Section------------
+print(container)
