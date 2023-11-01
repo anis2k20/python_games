63afac55dadf9c9f51838c1fbfe962b001aca588
Anis <anis2k20@gmail.com>
2023-11-01 14:20:05 +0600

    need to debug

--- FLAMES/main.py
+++ FLAMES/main.py
@@ -1,31 +1,45 @@
-# def remove_common_char(f,s):
-#     x = [i for i in f if i not in s]
-#     y = [j for j in s if j not in f]
-#     return x+y
-#
-# f_player_name = input("Enter 1st Person Name: ").lower()
-# s_player_name = input("Enter 2nd Person Name: ").lower()
-# unique_char = len(remove_common_char(f_player_name,s_player_name))
+def remove_common_char(f,s):
+    x = [i for i in f if i not in s]
+    y = [j for j in s if j not in f]
+    return x+y
+
+f_player_name = input("Enter 1st Person Name: ").lower()
+s_player_name = input("Enter 2nd Person Name: ").lower()
+unique_char = len(remove_common_char(f_player_name,s_player_name))
+COUNT = unique_char
 
 flames = ['F','L','A','M','E','S']
-COUNT = 4
-c = 0
-track = 0
-def cutting():
-    global c,track
-    for i in flames:
-        c += len(i)+track
-        try:
-            if c%3==0:
-                x = flames.pop(c)
-                track = len(flames[3:])
-        except IndexError:
-            print("Index out of range")
 
-while len(flames)>1:
-    cutting()
-    print(flames)
-    print(track)
+i = 0
+for j in range(len(flames)):
+    c,t=0,0
+    for char in flames:
+        t += len(char)
+        c = t + i
+        if c==COUNT:
+            index = flames.index(char)
+            i = len(flames[index+1:])
+            flames.pop(index)
+            break
+
+def rel(x):
+    if x =="E":
+        return("Enemies")
+    elif x == "F":
+        return("Friends")
+    elif x == "L":
+        return("Lovers")
+    elif x == "A":
+        return("Affectionate")
+    elif x == "M":
+        return("Marriage")
+    elif x == "S":
+        return("Siblings")
+
+print(f"Relationship Status : {rel(flames[0])}")
+
+
+
 
 
 
